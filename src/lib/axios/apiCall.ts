c2fe6ba32461d72191d79cd92eb3a60caff982a6
Arman <119550818+theaiemperor@users.noreply.github.com>
2025-08-17 11:43:48 +0530

    Simplified Axios api's

--- src/lib/axios/apiCall.ts
+++ src/lib/axios/apiCall.ts
@@ -1,145 +1,19 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios";
-import { IObj } from "../../types/common.js";
-import { APIResponse, APIResponseError, APIResponseSuccess } from "../../global/apiResponseTypes.js";
+import { AxiosInstance } from "axios";
+import { APIClient } from "./_apiClient.js";
 
 
 
-type HTTPMethods = 'get' | 'post' | 'put' | 'patch' | 'delete' | 'options' | 'head';
-
-type ComonMeta = { status: number, statusText: string };
-
-
-type HTTPImpl = <
-    ResData = IObj,
-    ErrData = string,
-    ResMeta extends object = ComonMeta,
-    ErrMeta extends object = ComonMeta
->(url: string, config?: AxiosRequestConfig) =>
-    Promise<APIResponse<ResData, ErrData, ResMeta, ErrMeta>>
+const client = new APIClient();
 
 
 
-export class APICall {
-    private instance: AxiosInstance;
-
-    constructor(instance?: AxiosInstance) {
-        this.instance = instance || axios.create();
-    }
-
-
-    private handleAxiosError<U, ErrMeta extends object>(err: any): APIResponseError<U, ErrMeta & ComonMeta> {
-        if (err instanceof AxiosError && err.response) {
-            const axiosErr = err.response.data;
-
-            return {
-                ...axiosErr,
-                meta: {
-                    ...axiosErr.meta,
-                    status: err.status,
-                    statusText: err.code
-                }
-            };
-        }
-
-        return {
-            success: false,
-            message: "Something went wrong",
-            data: {} as U,
-            meta: {
-                status: 0,
-                statusText: 'Unknown error'
-            } as ErrMeta & ComonMeta
-        }
-    }
-
-
-
-    //  Overloads for strong typings
-    private createHandler(method: "get" | "delete" | "head" | "options"): <
-        ResData = IObj,
-        ErrData = string,
-        ResMeta extends object = ComonMeta,
-        ErrMeta extends object = ComonMeta
-    >(
-        url: string,
-        config?: AxiosRequestConfig
-    ) => Promise<APIResponse<ResData, ErrData, ResMeta & ComonMeta, ErrMeta & ComonMeta>>;
-
-    private createHandler(method: "post" | "put" | "patch"): <
-        ResData = IObj,
-        ErrData = string,
-        ResMeta extends object = ComonMeta,
-        ErrMeta extends object = ComonMeta
-    >(
-        url: string,
-        data?: any,
-        config?: AxiosRequestConfig
-    ) => Promise<APIResponse<ResData, ErrData, ResMeta & ComonMeta, ErrMeta & ComonMeta>>;
-
-
-    // implementation of above method
-    private createHandler(method: HTTPMethods) {
-
-        const isDataMethod = ['post', 'put', 'patch'].includes(method);
-
-        if (isDataMethod) {
-            return async <ResData = IObj, ErrData = string, ResMeta extends object = IObj, ErrMeta extends object = IObj>(
-                url: string,
-                data?: any,
-                config?: AxiosRequestConfig
-            ): Promise<APIResponse<ResData, ErrData, ResMeta & ComonMeta, ErrMeta & ComonMeta>> => {
-
-                try {
-                    const res = await (this.instance as AxiosInstance)[method]<ResData>(url, data, config);
-                    return res.data as APIResponseSuccess<ResData, ResMeta & ComonMeta>;
-
-                } catch (err) {
-                    return this.handleAxiosError<ErrData, ErrMeta & ComonMeta>(err);
-                }
-            }
-        }
-
-
-        return async <ResData = IObj, ErrData = string, ResMeta extends object = IObj, ErrMeta extends object = IObj>(
-            url: string,
-            config?: AxiosRequestConfig
-        ): Promise<APIResponse<ResData, ErrData, ResMeta, ErrMeta>> => {
-            try {
-                const res = await (this.instance as AxiosInstance)[method]<ResData>(url, config);
-                return res.data as APIResponseSuccess<ResData, ResMeta>;
-
-            } catch (err) {
-                return this.handleAxiosError<ErrData, ErrMeta>(err);
-            }
-        }
-    }
-
-
-    call<Allowed extends HTTPMethods[]>(allowed?: Allowed): Allowed extends undefined
-        ? Record<HTTPMethods, HTTPImpl>
-        : Pick<Record<HTTPMethods, HTTPImpl>, Allowed[number]> {
-
-        const handlers = {
-            get: this.createHandler('get'),
-            post: this.createHandler('post'),
-            put: this.createHandler('put'),
-            patch: this.createHandler('patch'),
-            delete: this.createHandler('delete'),
-            options: this.createHandler('options'),
-            head: this.createHandler('head'),
-        };
-
-        if (!allowed) {
-            return handlers as any;
-        }
-
-
-        const filtered = Object.fromEntries(
-            Object.entries(handlers).filter(([method]) =>
-                allowed.includes(method as HTTPMethods)
-            )
-        );
-
-        return filtered as any;
-    }
+export function setAxiosInstance(instance: AxiosInstance) {
+    client.instance = instance;
 }
+
+
+export const getReq = client.createHandler('get');
+export const postReq = client.createHandler('post');
+export const putReq = client.createHandler('put');
+export const patchReq = client.createHandler('patch');
+export const deleteReq = client.createHandler('delete');
